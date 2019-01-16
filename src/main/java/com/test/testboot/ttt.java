98422dac36c33957925faca13a555c084dbf964f
yonghao <yongha@qq.com>
2019-01-16 19:10:30 +0800

    很好

--- src/main/java/com/test/testboot/ttt.java
+++ src/main/java/com/test/testboot/ttt.java
@@ -10,7 +10,7 @@ public class ttt {
         System.out.println("=====master branchfdsfds防守打f防守打法法git");
         System.out.println("：常以轩辕Rowboat为名混迹于江湖。前端路上 | PPT爱好");
 
-        //git branch
+        //git branch 很好呀
         System.out.println("=====test branch");
         System.out.println("=====test branch line 222 中文汉化");
 
